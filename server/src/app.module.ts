11bc15d92423171f463f83f005de77d30a4522d3
Matan Shidlov <91742238+mshidlov@users.noreply.github.com>
2022-01-02 15:39:06 +0200

    amplication-build-ckxxavai55880201s62dfj3s2e

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
-import { TodoModule } from "./todo/todo.module";
 import { BlaBlaModule } from "./blaBla/blaBla.module";
+import { HealthModule } from "./health/health.module";
 import { ACLModule } from "./auth/acl.module";
 import { AuthModule } from "./auth/auth.module";
-import { HealthModule } from "./health/health.module";
 import { SecretsManagerModule } from "./providers/secrets/secretsManager.module";
 import { MorganModule } from "nest-morgan";
 import { ConfigModule, ConfigService } from "@nestjs/config";
@@ -16,11 +15,10 @@ import { GraphQLModule } from "@nestjs/graphql";
   controllers: [],
   imports: [
     UserModule,
-    TodoModule,
     BlaBlaModule,
+    HealthModule,
     ACLModule,
     AuthModule,
-    HealthModule,
     SecretsManagerModule,
     MorganModule,
     ConfigModule.forRoot({ isGlobal: true }),
